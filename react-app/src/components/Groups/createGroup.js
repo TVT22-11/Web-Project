35dd9444cf723bd11faac53e2f64976198cfe3b0
iivarirantala <t2raii00@students.oamk.fi>
2023-11-15 15:28:13 +0200

    Updates on buttons

--- react-app/src/components/Groups/createGroup.js
+++ react-app/src/components/Groups/createGroup.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Groups.css';
-function createGroup (){
-    return (
-        <><div className='header'>
-<h3>Or</h3>
-        </div>
-        <div className='create-button'>
+function createGroup() {
+        return (
+                <><div className='header'>
+                        <h3>Or</h3>
+                </div>
+                        <div className='create-button'>
 
-                <button>Create Group</button>
-            </div></>    
+                                <button>Create Group</button>
+                        </div></>
 
-      
-    );
- }
- export default createGroup;
+
+        );
+}
+export default createGroup;
