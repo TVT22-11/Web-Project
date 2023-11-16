b6d8f9f02c0e46f632b971b80177b95e3b277c5d
iivarirantala <t2raii00@students.oamk.fi>
2023-11-16 12:35:10 +0200

    update

--- react-app/src/components/Groups/createGroup.js
+++ react-app/src/components/Groups/createGroup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Groups.css';
 import './creatingPage'
+
 function createGroup() {
         return (
                 <><div className='header'>
