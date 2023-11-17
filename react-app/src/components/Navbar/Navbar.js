bc2f5ef5667b36558c548782507850117edf8223
Veeko23 <122440628+Veeko23@users.noreply.github.com>
2023-11-17 16:35:21 +0200

    Updates
    News and Searchbar update

--- react-app/src/components/Navbar/Navbar.js
+++ react-app/src/components/Navbar/Navbar.js
@@ -6,7 +6,6 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
 
   const DDmenuClickHandler = () => {
     var dropdown = document.getElementById("myDropdown");
@@ -14,27 +13,7 @@ const Navbar = () => {
     console.log('Dropdown menu clicked!');
   };
 
-  const controlNavbar = () => {
-    if (typeof window !== 'undefined') { 
-      if (window.scrollY > lastScrollY) {
-        setShow(false); 
-      } else {
-        setShow(true);  
-      }
 
-      setLastScrollY(window.scrollY);
-    }
-  };
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', controlNavbar);
-
-      return () => {
-        window.removeEventListener('scroll', controlNavbar);
-      };
-    }
-  }, [lastScrollY]);
 
   return (
     <header>
--- react-app/src/components/Searchbar/searchbar.jsx
+++ react-app/src/components/Searchbar/searchbar.jsx
@@ -1,8 +1,8 @@
 
 import React, { useState } from 'react';
 import './search.css';
-
-
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState('');
 
@@ -21,15 +21,15 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <div className="search-bar">
-      <input
+    <div className="Search-bar">
+      <input className="Search-Input"
         type="text"
         placeholder="Search..."
         value={query}
         onChange={handleInputChange}
         onKeyPress={handleKeyPress}
       />
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSearch}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
     </div>
   );
 };
