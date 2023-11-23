2efff6ede7cb69e4bb8de47984cd2ae8be107d96
Moili20 <122986067+Moili20@users.noreply.github.com>
2023-11-23 16:30:43 +0200

    Movies sivun päivitystä API lisäys

--- react-app/src/components/Movies/Movies.js
+++ react-app/src/components/Movies/Movies.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import './Movies.css';
 import SearchBar from '../Searchbar/searchbar';
+const apiKey = process.env.REACT_APP_IMDB_API_BEARER_TOKEN;
+const apiUrl = process.env.REACT_APP_IMDB_API_URL;
+const apiImageBaseUrl= process.env.REACT_APP_IMDB_IMAGE_API_URL
 
-function Movies (){
+
+const Movies = ({language, page}) => {
+  const [movie, setMovies] = useState([]);
+
+useEffect(() =>{
+  const fetchData = async () => {
+    try {
+      const response = await fetch( `${apiUrl}movie/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+      {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        }
+        
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setMovies(data.results.slice(0, 4)); 
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  fetchData();
+}, []);
+  
   return (
-    <div className="movie-container">
-      <h1>Movies</h1>
-      <div>
-        <SearchBar/>
-      </div>
+    <div className='Movie-Container' >
+      {movie.map((movie) => (
+          <li className='Movie-Box' key={movie.id}>
+      <img  src={`${apiImageBaseUrl}${movie.poster_path}`}
+            style={{ maxWidth: '80%' }}/>
 
-      <div className="movie-box-container">
-        <div className="movie-box movie-box1">Movie 1</div>
-        <div className="movie-box movie-box2">Movie 2</div>
-        <div className="movie-box movie-box3">Movie 3</div>
-        <div className="movie-box movie-box4">Movie 4</div>
+      <div className='Movie-Desc'>
+      <h2>{movie.title}</h2>
+      <p>{movie.release_date}</p>
+      <p>Avarage vote: {movie.vote_average}</p>
+      <p>Vote count: {movie.vote_count}</p>
       </div>
+      </li>
+      
+        ))}
     </div>
   );
 };
