cb508d6ab79e6794eef2b12de00f91f99061f7c9
Moili20 <122986067+Moili20@users.noreply.github.com>
2023-11-24 15:26:16 +0200

    Movies sivun elokuvien asettelumuutoksia ja tesailua

--- react-app/src/components/Movies/Movies.js
+++ react-app/src/components/Movies/Movies.js
@@ -1,6 +1,6 @@
 import React,{ useState, useEffect } from 'react';
 import './Movies.css';
-import SearchBar from '../Searchbar/searchbar';
+import Searchbar from '../Searchbar/searchbar';
 const apiKey = process.env.REACT_APP_IMDB_API_BEARER_TOKEN;
 const apiUrl = process.env.REACT_APP_IMDB_API_URL;
 const apiImageBaseUrl= process.env.REACT_APP_IMDB_IMAGE_API_URL
@@ -12,7 +12,7 @@ const Movies = ({language, page}) => {
 useEffect(() =>{
   const fetchData = async () => {
     try {
-      const response = await fetch( `${apiUrl}movie/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+      const response = await fetch( `${apiUrl}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
       {
         method: 'GET',
         headers: {
@@ -27,7 +27,7 @@ useEffect(() =>{
       }
 
       const data = await response.json();
-      setMovies(data.results.slice(0, 5)); 
+      setMovies(data.results.slice(0, 6)); 
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -37,22 +37,31 @@ useEffect(() =>{
 }, []);
   
   return (
-    <div className='Movie-Container' >
-      {movie.map((movie) => (
-          <li className='Movie-Box' key={movie.id}>
-      <img  src={`${apiImageBaseUrl}${movie.poster_path}`}
-            style={{ maxWidth: '80%' }}/>
+    <div>
 
-      <div className='Movie-Desc'>
-      <h2>{movie.title}</h2>
-      <p>{movie.release_date}</p>
-      <p>Avarage vote: {movie.vote_average}</p>
-      <p>Vote count: {movie.vote_count}</p>
+        <Searchbar />
+
+      <div className='Movies-Container' >
+          {movie.map((movie) => (
+          <li className='Movie-Box' key={movie.id}>
+              <img  src={`${apiImageBaseUrl}${movie.poster_path}`}
+                style={{ maxWidth: '80%' }}
+              alt={movie.title}
+              />
+
+            <div className='Movies-Desc'>
+              <h2>{movie.title}</h2>
+              <p>Release:{movie.release_date}</p>
+              <p>Avarage vote: {movie.vote_average}</p>
+              <p>Vote count: {movie.vote_count}</p>
+            </div>
+          </li>
+        
+          ))}
       </div>
-      </li>
-      
-        ))}
+
     </div>
+    
   );
 };
 
