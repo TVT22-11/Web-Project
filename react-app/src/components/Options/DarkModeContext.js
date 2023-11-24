50b07dc26847d6845850a8e9d939c999b2f6f5d9
ATKKepa <112471010+ATKKepa@users.noreply.github.com>
2023-11-24 15:25:56 +0200

    darkmode globaliksi
    toimii kaikilla sivuilla

---
+++ react-app/src/components/Options/DarkModeContext.js
@@ -0,0 +1,31 @@
+// DarkModeContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(
+    JSON.parse(localStorage.getItem('darkMode')) || false
+  );
+
+  const toggleDarkMode = () => {
+    const newDarkMode = !isDarkMode;
+    setIsDarkMode(newDarkMode);
+    localStorage.setItem('darkMode', JSON.stringify(newDarkMode));
+    document.body.classList.toggle('dark-mode', newDarkMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+  if (!context) {
+    throw new Error('useDarkMode must be used within a DarkModeProvider');
+  }
+  return context;
+};
