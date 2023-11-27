b65153f08db51a63186bfb2c074ae6232d1702c0
iivarirantala <t2raii00@students.oamk.fi>
2023-11-27 14:17:15 +0200

    update

--- Backend/src/database_tools/pg_connection.js
+++ Backend/src/database_tools/pg_connection.js
@@ -6,7 +6,8 @@ const pgPool = new Pool({
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD
+    password: process.env.PG_PASSWORD,
+    ssl:true
 });
 
 pgPool.connect()
