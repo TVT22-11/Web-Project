9efd999cbe130cb2b771d9bb6d24a7b020780009
Veeko23 <122440628+Veeko23@users.noreply.github.com>
2023-11-28 15:09:34 +0200

    Search Filters

--- react-app/src/components/Searchbar/searchbar.jsx
+++ react-app/src/components/Searchbar/searchbar.jsx
@@ -12,6 +12,7 @@ const apiImageBaseUrl = process.env.REACT_APP_IMDB_IMAGE_API_URL;
 const SearchBar = () => {
   const [query, setQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedType, setSelectedType] = useState('/search/movie');
   const [showResults, setShowResults] = useState(false);
   const history = useNavigate();
   const searchResultsRef = useRef(null);
@@ -23,23 +24,29 @@ const SearchBar = () => {
   };
 
   const handleSearch = async () => {
-    const searchResults = await axios.get(
-      `${apiUrl}/search/movie?query=${query}`,
-      {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    const params = {
+      query,
+    };
+
+    const searchResults = await axios.get(`${apiUrl}${selectedType}`, {
+      params,
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    });
 
     setSearchResults(searchResults.data.results);
     setShowResults(true);
   };
 
-  const handleResultClick = (movieId) => {
+ const handleResultClick = (movieId) => {
     history(`/movie/${movieId}`);
     setShowResults(false);
+  };
+
+  const handleTypeChange = (e) => {
+    setSelectedType(e.target.value);
   };
 
   const handleKeyPress = (e) => {
@@ -76,17 +83,27 @@ const SearchBar = () => {
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
 
+      <select value={selectedType} onChange={handleTypeChange}>
+        <option value="/search/movie">All-Movies</option>
+        <option value="/search/tv">TV-Series</option>
+        <option value="/search/multi">Multi</option>
+        <option value="/search/person">People</option>
+
+      </select>
+
       {showResults && (
         <ul ref={searchResultsRef} className="Search-Results">
           {searchResults.map((result) => (
             <li className='search-result-container' key={result.id} onClick={() => handleResultClick(result.id)}>
-              <img src={`${apiImageBaseUrl}${result.poster_path}`}
-              
-            className='Movie-Image-SearchBar'
-            alt={result.title}
-            />
+                  {result.poster_path && (
+                <img
+                className='Movie-Image-SearchBar'
+                  src={`https://image.tmdb.org/t/p/w200${result.poster_path}`}
+                  alt={result.title}
+                />
+              )}
             <div className='results-description-container'>
-             <h1 className='result-title'>{result.title}</h1> 
+             <h1 className='result-title'>{result.title}{result.name}</h1> 
               <h2 className='result-release-date'>{result.release_date}</h2>
               </div>
             </li>
