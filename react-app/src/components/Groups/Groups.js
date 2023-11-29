1d31c79195d0eeb33ec6303332b3aa7fc6871207
iivarirantala <t2raii00@students.oamk.fi>
2023-11-29 13:56:34 +0200

    Groups update

--- react-app/src/components/Groups/Groups.js
+++ react-app/src/components/Groups/Groups.js
@@ -4,14 +4,17 @@ import YourGroups from './yourGroups';
 import SearchBar from '../Searchbar/searchbar';
 import AvailableGroups from './availableGroups';
 import CreateGroup from './createGroup';
+import { useUser } from '../User/UserContext';
+
 
 function Groups() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
-  // You can replace this function with your actual sign-in logic
+ 
   const handleSignIn = () => {
     // Implement your sign-in logic here
     // For example, set isSignedIn to true when the user signs in
+
     setIsSignedIn(true);
   };
 
