7a6baa2483c48644a3190e07485323294e53178b
iivarirantala <t2raii00@students.oamk.fi>
2023-12-07 16:16:37 +0200

    groups page back- & frontend updated

--- Backend/src/database_tools/groups_db.js
+++ Backend/src/database_tools/groups_db.js
@@ -1,51 +1,49 @@
 const pgPool = require('./pg_connection');
 
 const sql = {
-    Group : 'INSERT INTO party (name, description, isprivate) VALUES ($1, $2, $3)',
-    GET_GROUP_BY_ID: 'SELECT * FROM party WHERE name = $1',
-    GET_ALL_GROUPS: 'SELECT name, description FROM party',
-    Delete_Group: 'DELETE FROM party WHERE name = $1'
+  Group: 'INSERT INTO party (name, description, isprivate) VALUES ($1, $2, $3)',
+  GET_GROUP_BY_ID: 'SELECT * FROM party WHERE name = $1',
+  GET_ALL_GROUPS: 'SELECT name, description, isprivate FROM party', // Include isprivate in the SELECT statement
+  Delete_Group: 'DELETE FROM party WHERE name = $1',
 };
 
-async function createParty(name, description, isprivate){
-    const client = await pgPool.connect();
+async function createParty(name, description, isprivate) {
+  const client = await pgPool.connect();
 
-    try{
-        await client.query(sql.Group, [name, description, isprivate])
-    }finally{
-        client.release();
-    }  
+  try {
+    await client.query(sql.Group, [name, description, isprivate]);
+  } finally {
+    client.release();
+  }
 }
 
 async function getGroup(name) {
-    const client = await pgPool.connect();
-    
-    try {
-      if (name) {
-        let result = await pgPool.query(sql.GET_GROUP_BY_ID, [name]);
-        return result.rows.length > 0 ? result.rows : null;
-      }
-      throw new Error("Missing name parameter");
-  
-    } catch (err) {
-    
-      throw err;
-    } finally {
-      client.release();
-    }
-  }
+  const client = await pgPool.connect();
 
-  async function getAllGroups() {
-    const client = await pgPool.connect();
-    try {
-      let result = await pgPool.query(sql.GET_ALL_GROUPS);
-      return result.rows;
-    } catch (error) {
-      console.error("Error fetching all groups:", error);
-      throw error; 
-    } finally {
-      client.release();
+  try {
+    if (name) {
+      let result = await pgPool.query(sql.GET_GROUP_BY_ID, [name]);
+      return result.rows.length > 0 ? result.rows : null;
     }
+    throw new Error("Missing name parameter");
+  } catch (err) {
+    throw err;
+  } finally {
+    client.release();
   }
+}
 
-module.exports = {createParty, getGroup, getAllGroups};
+async function getAllGroups() {
+  const client = await pgPool.connect();
+  try {
+    let result = await client.query(sql.GET_ALL_GROUPS);
+    return result.rows; // This already returns an array of rows
+  } catch (error) {
+    console.error("Error fetching all groups:", error);
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+module.exports = { createParty, getGroup, getAllGroups };
