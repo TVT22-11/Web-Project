3a07d7f1d1bae56a74ed4e6e3c3d739970ab2893
Moili20 <122986067+Moili20@users.noreply.github.com>
2023-12-11 16:07:04 +0200

    review komponentti

--- react-app/src/components/Reviews/ReadReview.js
+++ react-app/src/components/Reviews/ReadReview.js
@@ -6,9 +6,8 @@ import { useParams } from "react-router-dom";
 function ReadReview() {
     const [reviews, setReviews] = useState([]);
     const { id } = useParams();
-    const [usernames, setUsernames] = useState({});
+    const [username, setUsername] = useState({});
     
-
     useEffect(() => {
         const fetchReview = async () => {
           try {
@@ -17,35 +16,8 @@ function ReadReview() {
             const reviewsData = reviewResponse.data.ReviewData;
             setReviews(reviewsData);
         
-            // Fetch usernames
-            const accountIds = reviewsData.map(review => review.id_account);
-            const usernamePromises = accountIds.map(async id_account => {
-              try {
-                const userResponse = await axios.get(`http://localhost:3001/account/personal`);
-                console.log(`User Response Data for id_account ${id_account}:`, userResponse.data);
-        
-                const userData = userResponse.data;
-                const username = userData.username;
-                return { id_account, username };
-              } catch (error) {
-                console.error(`Error fetching username for id_account ${id_account}:`, error);
-                return { id_account, username: null };
-              }
-            });
-        
-            const resolvedUsernames = await Promise.all(usernamePromises);
-        
-            const usernamesData = {};
-            resolvedUsernames.forEach(({ id_account, username }) => {
-              if (username !== null) {
-                usernamesData[id_account] = username;
-              } else {
-                console.error('No username found for id_account:', id_account);
-              }
-            });
-        
             // Update state with usernames
-            setUsernames(usernamesData);
+            setUsername();
           } catch (error) {
             console.error('Error fetching data:', error);
           }
@@ -60,16 +32,12 @@ function ReadReview() {
         <div className="review-box">
           <h2>Reviews</h2>
           <ul>
-          {reviews.map((review,index) => (
+          {reviews.map((review,) => (
           <li key={review.id_review}>
-            <p>ID: {review.id_review}</p>
-            <p>Stars: {review.stars}</p>
-            <p>Comment: {review.comment}</p>
-            <p>User ID: {usernames[review.id_account]}</p>
-              {usernames[review.id_account] && (
-                <p>Username: {usernames[review.id_account]}</p>
-              )}
-              </li>
+            <p>User: {review.username}</p>
+            <p>Rating: {review.stars}</p>
+            <p>Comment: {review.comment}</p>                            
+          </li>
             ))}
           </ul>
         </div>
