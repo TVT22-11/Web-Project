2546b68947802727ed2ecb7e393721b5ca97d0a4
Veeko23 <122440628+Veeko23@users.noreply.github.com>
2023-12-13 12:53:51 +0200

    Backend
    Valmis backend Viestin lähettämiselle ja hakemiselle.

--- Backend/src/database_tools/groups_db.js
+++ Backend/src/database_tools/groups_db.js
@@ -6,8 +6,35 @@ const sql = {
   GET_ALL_GROUPS: 'SELECT id_party, name, description, isprivate FROM party', // Include isprivate in the SELECT statement
   Delete_Group: 'DELETE FROM party WHERE name = $1',
   ADD_GROUP_MEMBER: 'INSERT INTO account_party_relation (id_account, id_party) VALUES ($1, $2)',
-  DELETE_GROUP_MEMBER: 'DELETE FROM account_party_relation WHERE id_account = $1 AND id_party = $2'
+  DELETE_GROUP_MEMBER: 'DELETE FROM account_party_relation WHERE id_account = $1 AND id_party = $2',
+  SEND_MESSAGE: 'INSERT INTO party_messages (id_account, id_party, messages) VALUES ($1, $2, $3)',
+  FETCH_MESSAGES: 'SELECT FROM party_messages WHERE id_party = $1',
 };
+
+async function sendMessage(id_account, id_party, messages) {
+  const client = await pgPool.connect();
+  try {
+    await client.query(sql.SEND_MESSAGE, [id_account, id_party, messages]);   
+  } finally {
+    client.release();
+  }
+}
+
+async function fetchMessages(id_party){
+  const client = await pgPool.connect();
+  try {
+    if (id_party) {
+      let result = await pgPool.query(sql.FETCH_MESSAGES, [id_party]);
+      return result.rows.length > 0 ? result.rows : null;
+    }
+    throw new Error("Missing id_party parameter");
+  } catch (err) {
+    throw err;
+  } finally {
+    client.release();
+  }
+}
+
 
 async function createParty(name, description, isprivate) {
   const client = await pgPool.connect();
@@ -70,4 +97,4 @@ async function getAllGroups() {
   }
 }
 
-module.exports = { createParty, getGroup, getAllGroups, addMember, deleteMember };
+module.exports = { createParty, getGroup, getAllGroups, addMember, deleteMember, sendMessage, fetchMessages };
--- Backend/src/routes/group.js
+++ Backend/src/routes/group.js
@@ -3,8 +3,33 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { authenticateToken } = require('../auth/auth');
-const { createParty, getGroup, getAllGroups, addMember, deleteMember} = require('../database_tools/groups_db');
+const { createParty, getGroup, getAllGroups, addMember, deleteMember, sendMessage, fetchMessages} = require('../database_tools/groups_db');
 const upload = multer({ dest: 'uploads/' });
+
+
+router.post('/send-message', upload.none(), async (req,res) =>{
+  const id_account = req.body.id_account;
+  const id_party = req.body.id_party;
+  const messages = req.body.messages;
+  try {
+    await sendMessage(id_account, id_party, messages);
+    res.status(200).json({ message: 'Post Send successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.get('/fetch-messages', async (req, res) =>{
+  const id_party = req.query.id_party;
+  try {
+    const messages = await fetchMessages(id_party);
+    res.status(200).json({ message: messages});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 router.post('/post', upload.none(), async (req, res) => {
   const name = req.body.name;
