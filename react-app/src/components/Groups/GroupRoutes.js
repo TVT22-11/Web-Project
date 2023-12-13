133f245c452f7149c04453c328fce757e29ec654
iivarirantala <t2raii00@students.oamk.fi>
2023-12-13 18:10:04 +0200

    Update GroupRoutes.js

--- react-app/src/components/Groups/GroupRoutes.js
+++ react-app/src/components/Groups/GroupRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreatingPage from './creatingPage';
 import Groups from './Groups';
+import ChatPage from './chatpage';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function GroupRoutes() {
@@ -12,6 +13,7 @@ return(
         <Route path="/" element={<Navigate to="Groups" />} />
         <Route path="Groups/*" element={<Groups />} />
         <Route path="CreatingPage" element={<CreatingPage />} />
+        <Route path="chatpage/:id_party" element={<ChatPage />} />
       </Routes>
     );  
     };
