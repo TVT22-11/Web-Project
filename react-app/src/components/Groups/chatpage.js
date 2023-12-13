2146d22e963a9963fe75aefe3cc09fd66cd8a492
Veeko23 <122440628+Veeko23@users.noreply.github.com>
2023-12-13 18:12:21 +0200

    Update chatpage.js

--- react-app/src/components/Groups/chatpage.js
+++ react-app/src/components/Groups/chatpage.js
@@ -10,17 +10,50 @@ function ChatPage() {
   const { accountID } = useUser();
   const navigate = useNavigate();
   const [idPartyToDelete, setIdPartyToDelete] = useState(null);
-  const {id_party} = useParams();
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const { id_party } = useParams();
 
   useEffect(() => {
-
     const params = new URLSearchParams(window.location.search);
-    const id_party = params.get('id_party');
-  
-    console.log('Party ID from URL:', id_party);
-  
-    setIdPartyToDelete(id_party);
+    const partyIdFromURL = params.get('id_party');
+
+    console.log('Party ID from URL:', partyIdFromURL);
+
+    setIdPartyToDelete(partyIdFromURL);
+    fetchMessages(partyIdFromURL);
   }, []);
+
+  const fetchMessages = async (partyId) => {
+    try {
+      const response = await axios.get(`${apiUrl}/messages/${partyId}`);
+      setMessages(response.data);
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+    }
+  };
+
+  const postMessage = async () => {
+    try {
+      const response = await axios.post(`${apiUrl}/messages`, {
+        id_account: accountID,
+        id_party: id_party,
+        message: newMessage,
+      });
+
+      console.log('Posted message:', newMessage);
+
+      if (response.status === 200) {
+        console.log('Successfully posted message');
+        fetchMessages(id_party); // Refresh messages after posting
+        setNewMessage(''); // Clear the message input
+      } else {
+        console.error('Failed to post message');
+      }
+    } catch (error) {
+      console.error('Error posting message:', error);
+    }
+  };
 
   const handleAddUser = () => {
     // Implement the logic for adding a user
@@ -35,7 +68,7 @@ function ChatPage() {
     }
 
     try {
-      const response = await axios.delete('http://localhost:3001/group/deleteMember', {
+      const response = await axios.delete(`${apiUrl}/group/deleteMember`, {
         data: {
           id_account: accountID,
           id_party: id_party,
@@ -47,8 +80,6 @@ function ChatPage() {
 
       if (response.status === 200) {
         console.log('Successfully deleted member');
-
-        
         navigate('/groups');
       } else {
         console.error('Failed to delete member');
@@ -58,13 +89,23 @@ function ChatPage() {
     }
   };
 
-
-  
   return (
     <div>
-        <button onClick={handleAddUser}>Add Member</button>
-        <button onClick={handleDeleteUser}>Delete Member</button>
-      
+      <div>
+        {messages.map((message) => (
+          <div key={message.id}>{message.content}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
+        placeholder="Type your message..."
+      />
+      <button onClick={postMessage}>Send Message</button>
+
+      <button onClick={handleAddUser}>Add Member</button>
+      <button onClick={handleDeleteUser}>Delete Member</button>
     </div>
   );
 }
